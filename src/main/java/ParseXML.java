88832c0ab89c28a3aec0405255b8bf5243a1306a
Husain Al-Mohssen <husain@alum.mit.edu>
2014-07-04 11:37:04 -0400

    semi-working, old Pair Model

--- src/main/java/ParseXML.java
+++ src/main/java/ParseXML.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.MapMaker;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
+import javafx.util.Pair;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -18,7 +19,7 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.ConcurrentMap;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -32,146 +33,138 @@ public class ParseXML {
 
     public static final String REC_END = "</REC>";
     public static final String REC_START = "<REC ";
-    static X x = new X();
+
+    final static Set<String> allSubjects = Sets.newConcurrentHashSet();
+    final static Set<String> allUids = Sets.newConcurrentHashSet();
+    final static Set<String> allCountries = Sets.newConcurrentHashSet();
+    final static ConcurrentMap<String, Set<String>> subjectsOfUid = new MapMaker()
+            .concurrencyLevel(16)
+            .initialCapacity(1000)
+            .makeMap();
+    final static ConcurrentMap<String, Set<String>> countryOfUid = new MapMaker()
+            .concurrencyLevel(16)
+            .initialCapacity(1000)
+            .makeMap();
+    final static ConcurrentMap<String, Set<String>> emailAddressOfUid = new MapMaker()
+            .concurrencyLevel(16)
+            .initialCapacity(1000)
+            .makeMap();
+    final static ConcurrentMap<String, Set<String>> woSNameOfUid = new MapMaker()
+            .concurrencyLevel(16)
+            .initialCapacity(1000)
+            .makeMap();
 
 
-    public static void main(String... args) throws Exception {
 
-        checkArgument(args.length >= 1, "what's the input file?");
+    public static void main(String... filenames) throws Exception {
 
-        final File inputFile = new File(args[0]);
-        final int numberOfWorkingFiles = Runtime.getRuntime().availableProcessors();
-        checkArgument(numberOfWorkingFiles > 1, "number of files to split has to be grater than 1");
-        final long minFileSize = inputFile.length() / numberOfWorkingFiles;
-        int outFileNum = 0;
-        BufferedWriter outputSink = newOutputFile(outFileNum);
-        long outPos = 0;
-        boolean write = false;
-        try (BufferedReader bufferedReader = Files.asCharSource(inputFile, Charset.defaultCharset()).openBufferedStream()) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.contains(REC_START)) {
-                    write = true;
-                }
-                if (write) {
-                    outputSink.write(line);
-                    outputSink.newLine();
-                    outPos += ((long) (line.length()) + 1L);
-                }
-                if (outPos > minFileSize && line.contains(REC_END)) {
-                    outputSink.close();
-                    outPos = 0;
-                    outFileNum++;
-                    outputSink = newOutputFile(outFileNum);
-                }
-                if (line.contains(REC_END)) {
-                    write = false;
-                }
-            }
-//            need to make sur ethe output stream closed before closing the input stream...
-            outputSink.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            err.println("problem parsing input file, file" + inputFile.getAbsolutePath());
+        checkArgument(filenames.length >= 1, "what's the input file?");
+        progressMonitor.start();
+
+        List<X> xes = Lists.newArrayList(filenames).parallelStream().map(ParseXML::toXMatrix).collect(Collectors.toList());
+
+        err.print("done parsing all files... summing");
+        final X finalX = new X(1900);
+        xes.forEach((x) -> finalX.add(x));
+
+
+        List<String> sortedSubjects = Lists.newArrayList();
+        sortedSubjects.addAll(allSubjects);
+        sortedSubjects.sort((c1, c2) -> c1.compareToIgnoreCase(c2));
+        sqlOut(sortedSubjects);
+        out.println("delete from x;");
+        for (int year : finalX.authorSubjectOfYear.keySet()) {
+            sqlOut(sortedSubjects, year, finalX.authorSubjectOfYear.get(year));
+        }
+        sqlOutAuthorCountryTable(finalX.authorCountry);
+        err.println("done! here are all the countries we saw:");
+        err.println(Joiner.on("\n").join(allCountries));
+        progressMonitor.interrupt();
+    }
+
+    private static void sqlOutAuthorCountryTable(final X.PairHistogram authorCountry) {
+
+    }
+
+
+    private static void sqlOut(final List<String> sortedSubjects, final int year, X.PairHistogram authorSubject) {
+        for (Map.Entry<Pair<String, String>, Double> entry : authorSubject.histogram.entrySet()) {
+            StringBuffer sql = new StringBuffer("INSERT INTO x\n");
+            final String author = entry.getKey().getKey();
+            final String subject = entry.getKey().getValue();
+            final int sPos = sortedSubjects.indexOf(subject);
+            sql.append("(author, year, s" + sPos + ")\n");
+            sql.append("VALUES ('" + author.replace("'","\\'") + "', " + year + ", " + entry.getValue() + ");");
+            out.println(sql);
         }
 
-
-        parseTmpFilesInParallel(outFileNum);
-
-        csvDump(countryOfUid);
-
     }
 
-
-
-
-
-    private static void csvDump(final ConcurrentMap<String, Set<String>> emailAddressOfUid) {
-        emailAddressOfUid.forEach((s, strings) -> {
-            out.print(s + '|');
-            out.println(Joiner.on('|').join(strings));
-        });
-    }
-
-    private static void parseTmpFilesInParallel(final int lastFileNum) throws Exception {
-        IntStream.rangeClosed(0, lastFileNum).parallel()
-                .mapToObj(ParseXML::newTmpReader)
-                .forEach(ParseXML::processTmpInputBuffer);
-    }
-
-    private static void processTmpInputBuffer(BufferedReader tmpFileReader) {
-        StringBuffer recordTxt = new StringBuffer(44000);// average size of document is about 22k
-        String line;
-        try {
-            while ((line = tmpFileReader.readLine()) != null) {
-                recordTxt.append(line);
-                recordTxt.append('\n');
-                if (line.contains(REC_END)) {//done with this record
-                    processRecord(recordTxt.toString());
-                    recordTxt.setLength(0);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            err.println("IO error on thread " + Thread.currentThread().getName());
-            Throwables.propagate(e);
+    private static void sqlOut(final List<String> sortedSubjects) {
+        out.println("delete from subject_s;");
+        for (int i = 0; i < sortedSubjects.size(); i++) {
+            out.println("INSERT INTO subject_s (subject, s) VALUES ('" + sortedSubjects.get(i) + "'," + i + ");");
         }
     }
 
 
-    private static java.io.BufferedWriter newOutputFile(int outFileNum) throws FileNotFoundException {
-        return Files.newWriter(getFile(outFileNum), Charset.defaultCharset());
+    private static X toXMatrix(final String filename) {
+        final File inputFile = new File(checkNotNull(filename));
+        int year = Integer.parseInt(filename.substring(3, 7));
+        final X x = new X(year);
+        final X result= getXFromBufferedReader(bufferedReaderOfFile(inputFile), x, year);
+        err.println("\t done processing file="+filename);
+        return result;
     }
 
-    private static File getFile(int outFileNum) {
-        return new File("/tmp/" + Integer.toString(outFileNum) + ".xml");
-    }
 
-    private static BufferedReader newTmpReader(int i) {
+    private static BufferedReader bufferedReaderOfFile(final File file) {
         BufferedReader bufferedReader = null;
         try {
-            bufferedReader = Files.newReader(getFile(i), Charset.defaultCharset());
+            bufferedReader = Files.newReader(checkNotNull(file), Charset.defaultCharset());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return checkNotNull(bufferedReader);
     }
 
+    private static X getXFromBufferedReader(BufferedReader tmpFileReader, final X Xbatch, final int year) {
+        StringBuffer recordTxt = new StringBuffer(44000);// average size of document is about 22k
+        String line;
+        try {
+            while ((line = tmpFileReader.readLine()) != null) {
+                if (line.contains(REC_START)) {
+                    recordTxt.setLength(0);
+                }
+                recordTxt.append(line);
+                recordTxt.append('\n');
+                if (line.contains(REC_END)) {//done with this record
+                    processRecord(Xbatch, recordTxt.toString(), year);
+                    recordTxt.setLength(0);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            err.println("IO error on processingThread=" + Thread.currentThread().getName());
+            Throwables.propagate(e);
+        }
+        return Xbatch;
+    }
+
 
 //    key data structures, concurrent!:
-
-    static Set<String> allSubjects = Sets.newConcurrentHashSet();
-    static Set<String> allUids = Sets.newConcurrentHashSet();
-    static Set<String> allCountries = Sets.newConcurrentHashSet();
-    static Set<String> allEmailAddresses = Sets.newConcurrentHashSet();
-    static Set<String> allAuthorNames = Sets.newConcurrentHashSet();
-    static ConcurrentMap<String, Set<String>> subjectsOfUid = new MapMaker()
-            .concurrencyLevel(8)
-            .initialCapacity(1000)
-            .makeMap();
-    static ConcurrentMap<String, Set<String>> countryOfUid = new MapMaker()
-            .concurrencyLevel(8)
-            .initialCapacity(1000)
-            .makeMap();
-    static ConcurrentMap<String, Set<String>> emailAddressOfUid = new MapMaker()
-            .concurrencyLevel(8)
-            .initialCapacity(1000)
-            .makeMap();
-    static ConcurrentMap<String, Set<String>> woSNameOfUid = new MapMaker()
-            .concurrencyLevel(8)
-            .initialCapacity(1000)
-            .makeMap();
 
 
     /**
      * key processing Method
      *
+     * @param x         where we add our results
      * @param xmlRecord string of the xml representing the TR data of a publication
      */
-    private static void processRecord(String xmlRecord) throws Exception {
+    private static void processRecord(final X x, String xmlRecord, int year) throws Exception {
 //       bunch of sanity checks:
         checkArgument(xmlRecord.contains(REC_START) && xmlRecord.contains(REC_END), "valid xml file???!!!\n\n" + xmlRecord);
-        checkArgument(xmlRecord.contains("<UID>WOS:"), "valid xml file???!!!\n\n" + xmlRecord);
+        checkArgument(xmlRecord.contains("<UID>"), "valid xml file???!!!\n\n" + xmlRecord);
 
         InputSource source = new InputSource(new StringReader(xmlRecord));
 
@@ -191,8 +184,6 @@ public class ParseXML {
         allUids.add(uid);
         allSubjects.addAll(subjects);
         allCountries.addAll(countries);
-        allEmailAddresses.addAll(emailAddresses);
-        allAuthorNames.addAll(names);
 
         //the following assumes we only see a UID once in the different threads so we don't have to worry about syncing
         countryOfUid.put(uid, countries);
@@ -200,7 +191,7 @@ public class ParseXML {
         emailAddressOfUid.put(uid, emailAddresses);
         woSNameOfUid.put(uid, names);
 
-        x.appnedToExportMatrix(subjects, countries, names);
+        x.appendToMatrix(subjects, countries, names, year);
     }
 
     private static Set<String> setOfStringsFromXPath(XPath xpath, final String xpathPattern, final Document document) throws XPathExpressionException {
@@ -218,5 +209,22 @@ public class ParseXML {
         return (propertySet);
 
     }
+
+    static Thread progressMonitor = new Thread() {
+        @Override
+        public void run() {
+            while (true) {
+                err.print("\tAvailable memory: " + Runtime.getRuntime().totalMemory() / (1024 * 1024 * 1024) + " GB");
+                err.println("\tprogress...\t " + allUids.size() / 1_000_000 + "Million Documents processed:");
+                err.println(allCountries.size() + "countries\t" + allSubjects.size() + "subjects, and\t" + woSNameOfUid.size() + "authors");
+                try {
+                    Thread.sleep(1000 * 5 * 60);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
 
 }
--- src/main/java/X.java
+++ src/main/java/X.java
@@ -1,8 +1,13 @@
 import com.google.common.base.Objects;
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.MapMaker;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import javafx.util.Pair;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -14,44 +19,75 @@ import static java.lang.System.err;
  */
 public class X {
     Logger log = Logger.getAnonymousLogger();
-    private Table<String, String, Double> nameSubjectTable = HashBasedTable.create();
-    private Table<String, String, Integer> nameCountryTable = HashBasedTable.create();
 
-    public X() {
+    Map<Integer, PairHistogram> authorSubjectOfYear = Maps.newConcurrentMap();
+    final PairHistogram authorCountry=new PairHistogram();
+
+    private X() {
+    }
+
+    public X(final int year) {
+        authorSubjectOfYear.put(year,new PairHistogram());
     }
 
 
-    public void appnedToExportMatrix(Set<String> subjects, Set<String> countries, Set<String> names) {
-        checkArgument(names.size() > 0 && subjects.size() > 0);
-        final double inc = 1.0 / ((double) names.size() * subjects.size());
-        for (String name : names) {
+    public void appendToMatrix(Set<String> subjects, Set<String> countries, Set<String> authors, final int year) {
+        double inc = 1.0 / ((double) authors.size() * subjects.size());
+        for (String authorName : authors) {
             for (String subject : subjects) {
-                incrementXby(name, subject, inc);
+                authorSubjectOfYear.get(year).incPairBy(authorName, subject, inc);
             }
         }
-        if (countries.size() == 0) {
-            log.fine("no found ... ");
-            err.print("DEBUG: DELME no countries");
-            return;
-        }
-        for (String name : names) {
+        inc = 1.0 / ((double) authors.size() * countries.size());
+        for (String authorName : authors) {
             for (String country : countries) {
-                incrementAX(name, country);
+                authorCountry.incPairBy(authorName, country, inc);
             }
         }
     }
 
-    private void incrementAX(final String name, final String country) {
-//        int current = getNameCountry(name, country);
-//        nameSubjectTable.put(name, subject, current + 1);
+    /**
+     * meant to run in series
+     * @param otherX
+     */
+    synchronized public void add(final X otherX) {
+        //first add the year histogram
+        Set<Integer> otherYears = otherX.authorSubjectOfYear.keySet();
+        Set<Integer> unionYears=Sets.union(this.authorSubjectOfYear.keySet(), otherYears);
+        Set<Integer> intersectionYears=Sets.intersection(this.authorSubjectOfYear.keySet(), otherYears);
+        for(int year: unionYears){
+            final PairHistogram otherXPairHistogramOfYear = otherX.authorSubjectOfYear.get(year);
+            if(intersectionYears.contains(year)){//need to add
+                this.authorSubjectOfYear.get(year).appendAndAdd(otherXPairHistogramOfYear);
+            }else if(otherYears.contains(year)){//copy them over from the other year
+                this.authorSubjectOfYear.put(year, otherXPairHistogramOfYear);
+            }
+        }
+        //now add the author country histogram
+        this.authorCountry.appendAndAdd(otherX.authorCountry);
     }
 
-    synchronized private void incrementXby(final String name, final String subject, final double inc) {
-        double current = getNameSubject(name, subject);
-        nameSubjectTable.put(name, subject, current + inc);
-    }
+    class PairHistogram {
+        ConcurrentMap<Pair<String, String>, Double> histogram = new MapMaker()
+                .concurrencyLevel(16)
+                .initialCapacity(1000)
+                .makeMap();
 
-    private double getNameSubject(final String name, final String subject) {
-        return Objects.firstNonNull(nameSubjectTable.get(name, subject), 0.0);
+        public void incPairBy(String s1, String s2, final double delta) {
+            final Pair key = new Pair(s1, s2);
+            histogram.compute(key, (k, v) -> (v == null) ? delta : v + delta);
+
+        }
+
+        /**
+         * not meant to run in parallel
+         * @param otherHistogram
+         */
+        synchronized void appendAndAdd(final PairHistogram otherHistogram) {
+            for(Pair<String,String> oKey:otherHistogram.histogram.keySet()){
+                final Double otherVal = otherHistogram.histogram.get(oKey);
+                this.histogram.compute(oKey, (k, v) -> (v == null) ? otherVal : v + otherVal);
+            }
+        }
     }
 }
